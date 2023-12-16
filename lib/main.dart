9fcf3408ad4a505ee3fa681801a3a3f5214b46b2
Mayeavx <mayoodukoya@gmail.com>
2023-12-17 00:51:03 +0100

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Vallue Notifier Playground',
+      title: 'Value Notifier Playground',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
